7121ee8f7c78289d61fd9c2eb5ff2f759076d4a4
SK Kamrej <skkamrej786@gmail.com>
2026-08-19 01:49:58 +0530

    Update site title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -78,7 +78,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "SK KAMREJ | PORTFOLIO" },
+      { title: "SK KAMREJ | ASPIRING DEVELOPER • RESEARCHER" },
       {
         name: "description",
         content:
